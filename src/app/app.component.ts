b80631ea1d8c420d31838f12f92fbd6fd6e9a82e
knowhatelse <kenan.kajtazovic.01@gmail.com>
2024-05-15 11:06:18 +0200

    Implemented the media-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabComponent } from './presentation/components/tab/tab.component';
 import { SearchComponent } from './presentation/components/search/search.component';
+import { MediaContainerComponent } from './presentation/components/media/media-container/media-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TabComponent, SearchComponent],
+  imports: [RouterOutlet,TabComponent, SearchComponent, MediaContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
