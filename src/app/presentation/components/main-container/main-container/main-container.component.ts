495fca2091e2622caf1f8a54343d84fbdf649436
knowhatelse <kenan.kajtazovic.01@gmail.com>
2024-05-17 04:32:16 +0200

    Fixed the layout of the component

--- src/app/presentation/components/main-container/main-container/main-container.component.ts
+++ src/app/presentation/components/main-container/main-container/main-container.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { TabComponent } from "../../tab/tab.component";
 import { SearchComponent } from "../../search/search.component";
 import { MediaContainerComponent } from "../../media/media-container/media-container.component";
+import { MediaDetailComponent } from "../../media/media-detail/media-detail.component";
 
 @Component({
     selector: 'app-main-container',
     standalone: true,
     templateUrl: './main-container.component.html',
     styleUrl: './main-container.component.css',
-    imports: [TabComponent, SearchComponent, MediaContainerComponent]
+    imports: [TabComponent, SearchComponent, MediaContainerComponent, MediaDetailComponent]
 })
 export class MainContainerComponent {
 
