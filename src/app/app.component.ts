5a4b4674c0d3b0daf2b90c4beb6f8839436065ad
knowhatelse <kenan.kajtazovic.01@gmail.com>
2024-05-17 02:33:52 +0200

    Implemented the main container component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TabComponent } from './presentation/components/tab/tab.component';
-import { SearchComponent } from './presentation/components/search/search.component';
-import { MediaContainerComponent } from './presentation/components/media/media-container/media-container.component';
-import { MediaDetailComponent } from './presentation/components/media/media-detail/media-detail.component';
+import { MainContainerComponent } from "./presentation/components/main-container/main-container/main-container.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,TabComponent, SearchComponent, MediaContainerComponent, MediaDetailComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MainContainerComponent]
 })
 export class AppComponent {
   title = 'rubicon-assignment';
